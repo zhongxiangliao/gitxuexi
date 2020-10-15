4cdc8bf4683df9d52ee4f07318c8fc801badef26
lzx <1771607935@qq.com>
2020-10-15 17:09:14 +0800

    将已经push到github上的文件helloworld.java修改后，将修改后的文件重新commit并push到github的本项目中，实现修改同步

--- src/main/java/com/example/gitxuexi/helloworld.java
+++ src/main/java/com/example/gitxuexi/helloworld.java
@@ -2,6 +2,6 @@ package com.example.gitxuexi;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello");
     }
 }
